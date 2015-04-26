dfa17d5db13c059958b18eee8a9a03da2bce7510
wolfskaempf <tom@wolfskaempf.de>
2015-04-26 15:13:40 +0200

    New committee list now has a URL

--- src/ga_statistics/urls.py
+++ src/ga_statistics/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'web.views.home', name='home'),
-    url(r'^statistics/$', 'stathandler.views.statistics', name='statistics'),
-    url(r'^statistics/(?P<pk>\d+)/$', 'stathandler.views.statistics', name='statistics'),
+    url(r'^statistics/$', 'stathandler.views.committee_list', name='statistics'),
+    url(r'^statistics/(?P<pk>\d+)/$', 'stathandler.views.committee_single', name='statistics'),
     url(r'^submit/', 'stathandler.views.submit', name='submit'),
 ]
