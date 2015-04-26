c2c30f6be4b45364ab1ed8ad414491a1d9fa94e9
wolfskaempf <tom@wolfskaempf.de>
2015-04-26 13:41:33 +0200

    Added new Committee and Committee Statistic Models
    Please note that these are not finished yet and only contain basic
    information

--- src/stathandler/models.py
+++ src/stathandler/models.py
@@ -5,3 +5,21 @@ from django.db import models
 
 class DummyData(models.Model):
     number = models.CharField(max_length=100)
+
+
+class Committee(models.Model):
+    acronym = models.CharField(max_length=10)
+    longName = models.CharField(max_length=200)
+    topic = models.TextField()
+    gaPosition = models.CharField(max_length=3, null=True)
+
+    def __unicode__(self):
+        return self.acronym
+
+
+class CommitteeStatistic(models.Model):
+    committee = models.ForeignKey(Committee)
+    funFactDummy = models.TextField()
+
+    def __unicode__(self):
+        return self.committee.acronym
