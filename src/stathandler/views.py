93a73482af27991d0eac93d61e3d185a7cd17c74
wolfskaempf <tom@wolfskaempf.de>
2015-04-26 15:14:05 +0200

    Now there are committee_list and committee_single views

--- src/stathandler/views.py
+++ src/stathandler/views.py
@@ -6,13 +6,23 @@ from django.contrib.auth.decorators import login_required
 from .models import DummyData, Committee, CommitteeStatistic
 from .forms import DummyForm
 
-def statistics(request):
+def committee_list(request):
+    """ This view serves the list of all the committees displayed after clicking Statistics """
     committee = Committee.objects.all()
-    committeestatistic = CommitteeStatistic.objects.all()
-    return render(request, 'statistics.html', {'committee': committee, 'committeestatistic': committeestatistic})
+    return render(request, 'committee_list.html', {'committee': committee})
+
+def committee_single(request, pk):
+    """ This view serves the actual statistics for each proposing committee """
+    committees = Committee.objects.all()
+
+    committee = Committee.objects.get(pk=pk)
+
+    return render(request, 'committee_single.html', {'committee': committee, 'pk': pk})
+
 
 @login_required(login_url = '/admin/')
 def submit(request):
+    """ This view requires the user to be logged in. When it's finished, it'll show a form which can be used to submit the data which is displayed in the committee_single view """
     form = DummyForm(request.POST or None)
 
     if form.is_valid():
