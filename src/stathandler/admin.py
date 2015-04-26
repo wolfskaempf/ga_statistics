a31b25c2ea9fb83e20b69ac0682b7aac06a8c0fb
wolfskaempf <tom@wolfskaempf.de>
2015-04-26 13:39:44 +0200

    Registered New Committee Models in the Admin

--- src/stathandler/admin.py
+++ src/stathandler/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import DummyData
+from .models import DummyData, Committee, CommitteeStatistic
 
 admin.site.register(DummyData)
+admin.site.register(Committee)
+admin.site.register(CommitteeStatistic)
