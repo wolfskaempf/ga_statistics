2c972234583472a043a8e88ad0a82c5316827c35
wolfskaempf <tom@wolfskaempf.de>
2015-04-28 20:57:22 +0200

    Enriched Admin Interface with new list views and search fields for the Committee Model

--- src/stathandler/admin.py
+++ src/stathandler/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import DummyData, Committee, CommitteeStatistic
 
+
+
+class CommitteeAdmin(admin.ModelAdmin):
+    list_display = ['acronym', 'topic', 'gaPosition']
+    search_fields = ['acronym', 'topic', 'gaPosition']
+
+
 admin.site.register(DummyData)
-admin.site.register(Committee)
+admin.site.register(Committee, CommitteeAdmin)
 admin.site.register(CommitteeStatistic)
