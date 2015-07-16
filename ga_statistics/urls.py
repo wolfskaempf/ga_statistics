d6b7ea226a91d95cdad5e21e264e3843549abef1
wolfskaempf <tom@wolfskaempf.de>
2015-07-16 22:25:44 +0200

    renamed "statistics" to "committees"

--- ga_statistics/urls.py
+++ ga_statistics/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'web.views.home', name='home'),
-    url(r'^statistics/$', 'stathandler.views.committee_list', name='statistics'),
-    url(r'^statistics/(?P<pk>\d+)/$', 'stathandler.views.committee_single', name='statistics'),
+    url(r'^statistics/$', 'stathandler.views.committee_list', name='committee_list'),
+    url(r'^statistics/(?P<pk>\d+)/$', 'stathandler.views.committee_single', name='committee_single'),
     url(r'^submit/', 'stathandler.views.submit', name='submit'),
     url(r'^login/$', 'ga_auth.views.ga_login', name='login'),
     url(r'^logout/$', 'ga_auth.views.ga_logout', name='logout'),
