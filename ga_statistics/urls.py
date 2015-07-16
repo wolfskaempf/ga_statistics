411c96f0514e32069e8f9c56ab5d2a4f6985fac5
wolfskaempf <tom@wolfskaempf.de>
2015-07-16 22:33:49 +0200

    changed url scheme to reflect renamed navigation

--- ga_statistics/urls.py
+++ ga_statistics/urls.py
@@ -10,8 +10,10 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'web.views.home', name='home'),
-    url(r'^statistics/$', 'stathandler.views.committee_list', name='committee_list'),
-    url(r'^statistics/(?P<pk>\d+)/$', 'stathandler.views.committee_single', name='committee_single'),
+    url(r'^statistics/$', 'stathandler.views.committee_list', name='committee_list_legacy'),
+    url(r'^statistics/(?P<pk>\d+)/$', 'stathandler.views.committee_single', name='committee_single_legacy'),
+    url(r'^committees/$', 'stathandler.views.committee_list', name='committee_list'),
+    url(r'^committee/(?P<pk>\d+)/$', 'stathandler.views.committee_single', name='committee_single'),
     url(r'^submit/', 'stathandler.views.submit', name='submit'),
     url(r'^login/$', 'ga_auth.views.ga_login', name='login'),
     url(r'^logout/$', 'ga_auth.views.ga_logout', name='logout'),
