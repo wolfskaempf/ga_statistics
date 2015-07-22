434f866cbdf4b7e5ce7bfc99dd8d6c178b707232
wolfskaempf <tom@wolfskaempf.de>
2015-07-23 00:44:51 +0300

    fixed a bug that allowed only one statistic to be submitted per committee

--- stathandler/models.py
+++ stathandler/models.py
@@ -16,7 +16,7 @@ class Committee(models.Model):
     topic = models.TextField()
 
     # This is the position of the committe in GA. The committee with the number 0.1 will be displayed first, the one with number 0.1 second and so on. The tenth committee would be 1 and the eleventh 1.1
-    gaPosition = models.IntegerField(null=True)
+    gaPosition = models.CharField(max_length=3, null=True)
 
     def __unicode__(self):
         # This function defines what the object will return when it's viewed as a whole. This is especially practical for list views.
@@ -31,13 +31,13 @@ class CommitteeStatistic(models.Model):
     # This model will contain the statistical information about the committee it's linked to by the OneToOneField. If AGRI says 5 things about clause 3 that ENVI wrote, this should be saved here with ENVI being the OneToOneField and AGRI the ForeignKey.
 
     # This is the committee who is currently presenting its resolution
-    proposingCommittee = models.OneToOneField(Committee)
+    proposingCommittee = models.ForeignKey(Committee)
 
     # This is the committee who is currently commenting on something / making a point
     speakingCommittee = models.ForeignKey(Committee, related_name="+") #The related_name="+" meand that no backwards relation will be created for this ForeignKey. You can not call a set of this in relation to Committe as a cause of this.
 
     # This is the type of point that has been made. It can either be a DirectResponse or a normal Point at the moment. Of course, there are other types of points but these normally don't contribute to the debate which is why it might be good to leave them out.
-    pointType = models.CharField(max_length=20, choices=(("Point", "Point"), ("DirectResponse", "Direct Response")))
+    pointType = models.CharField(max_length=20, choices=(("Point", "Point"), ("DirectResponse", "Direct Response")), default="Point")
 
     # This field stores the contents of what has been said. It should be a (brief) resume of the point that has been made.
     pointResume = models.TextField()
