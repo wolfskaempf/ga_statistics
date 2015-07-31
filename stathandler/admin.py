0cdf5d5ec9192a6c39a19b01bce59fe86bf4ef91
wolfskaempf <tom@wolfskaempf.de>
2015-07-31 12:10:27 +0300

    added the proper CommitteeAdmin class

--- stathandler/admin.py
+++ stathandler/admin.py
@@ -7,12 +7,17 @@ from .models import Committee, CommitteeStatistic
 
 class CommitteeAdmin(admin.ModelAdmin):
     # This defines what the Committee Model should look like in the Django Admin
-    list_display = ['acronym', 'topic', 'gaPosition']
-    search_fields = ['acronym', 'topic', 'gaPosition']
+    list_display = ["acronym", "topic", "gaPosition"]
+    search_fields = ["acronym", "topic", "gaPosition"]
+
+class CommitteeStatisticAdmin(admin.ModelAdmin):
+    # This defines what the Committee Model should look like in the Django Admin
+    list_display = ["proposingCommittee", "speakingCommittee", "pointType", "pointResume"]
+    search_fields = ["proposingCommittee", "speakingCommittee", "pointType", "pointResume"]
 
 
 
 # The following lines are responsible for making the Model Data that's inside the database available in the Django admin
 
 admin.site.register(Committee, CommitteeAdmin)
-admin.site.register(CommitteeStatistic)
+admin.site.register(CommitteeStatistic, CommitteeStatisticAdmin)
